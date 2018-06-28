9b912f2aa4215e1ac98e27690a1299ec44f84114
Adam P. Krieger <apkriege@svsu.edu>
2018-06-28 14:27:58 -0400

    starting to move stuff in layout

--- public/js/app.jsx
+++ public/js/app.jsx
@@ -1,4 +1,25 @@
 // CURRENT CONDITIONS
+class Location extends React.Component {
+  constructor() {
+    super()
+  }
+
+  // async componentDidMount() {
+    // let res = await fetch('/current');
+    // let data = await res.json();
+    // console.log(data);
+  // }
+
+  render() {
+    return (
+      <div className="location">
+        <span>Lake Tahoe, UT</span>
+      </div>
+    )
+  }
+}
+
+// CURRENT CONDITIONS
 class Current extends React.Component {
   constructor() {
     super()
@@ -13,11 +34,33 @@ class Current extends React.Component {
   render() {
     return (
       <div className="current">
+        <div className="temp">75°</div>
+      </div>
+    )
+  }
+}
+
+// FIVEDAY CONDITIONS
+class FiveDay extends React.Component {
+  constructor() {
+    super()
+  }
+
+  // async componentDidMount() {
+    // let res = await fetch('/current');
+    // let data = await res.json();
+    // console.log(data);
+  // }
+
+  render() {
+    return (
+      <div className="fiveday">
         something
       </div>
     )
   }
 }
+
 
 
 // 5 DAY FORECAST
@@ -37,7 +80,9 @@ class App extends React.Component {
   render() {
     return (
       <div className="weather">
-        <Current />
+        <div className="top"><Location /></div>
+        <div className="middle"><Current /></div>
+        <div className="bottom"><FiveDay /></div>
       </div>
     )
   }
